5724427a43acec10c9181e1ab683c27661959aca
Noobron <georgemanjooran08@gmail.com>
2022-03-08 21:50:48 +0530

    Added typescript support to cli and local-api

---
+++ packages/local-api/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = () => {
+    console.log("local-api server started\nServer is listening");
+};
--- packages/local-api/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = () => {
-  console.log("local-api server started\nServer is listening...");
-};
