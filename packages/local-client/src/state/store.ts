d066e9ff7a1ccbee338909bfe286d7aefe92b217
Noobron <georgemanjooran08@gmail.com>
2022-03-08 19:36:19 +0530

    Minor cleanup

--- packages/local-client/src/state/store.ts
+++ packages/local-client/src/state/store.ts
@@ -1,45 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import { ActionType } from "./action-types";
-import { defaultBlockContent } from "./block";
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 
 export default store;
-
-store.dispatch({
-  type: ActionType.INSERT_BLOCK_AFTER,
-  payload: {
-    id: null,
-    type: "code",
-    extension: defaultBlockContent,
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_BLOCK_AFTER,
-  payload: {
-    id: null,
-    type: "text",
-    extension: defaultBlockContent,
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_BLOCK_AFTER,
-  payload: {
-    id: null,
-    type: "code",
-    extension: defaultBlockContent,
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_BLOCK_AFTER,
-  payload: {
-    id: null,
-    type: "text",
-    extension: defaultBlockContent,
-  },
-});
